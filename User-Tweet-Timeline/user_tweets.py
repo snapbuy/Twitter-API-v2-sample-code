34f1cf99579a4e3cf4d0f130312ef758295824c1
snapbuy <snapbuy@naver.com>
2022-07-05 14:06:14 +0900

    bearer_token

--- User-Tweet-Timeline/user_tweets.py
+++ User-Tweet-Timeline/user_tweets.py
@@ -9,7 +9,7 @@ bearer_token = os.environ.get("BEARER_TOKEN")
 
 def create_url():
     # Replace with user ID below
-    user_id = 2244994945
+    user_id = 2475502
     return "https://api.twitter.com/2/users/{}/tweets".format(user_id)
 
 
